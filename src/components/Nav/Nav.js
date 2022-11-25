2976fa60fed7c11da3cf9885e58a09f0a04b3492
rameesa <websoulrameesa@gmail.com>
2022-11-25 10:40:30 +0530

    make it better

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useState } from 'react'
 import './Nav.css'
-import {AiOutlineHome} from 'react-icons/ai'
-import {AiOutlineUser} from 'react-icons/ai'
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+
 import {BsBookmark} from 'react-icons/bs'
 import {RiServiceLine} from 'react-icons/ri'
 import {BiMessageSquare} from 'react-icons/bi'
@@ -10,8 +11,8 @@ const Nav = () => {
   const [navactive,setNavactive]=useState('#')
   return (
     <nav>
-      <a href="#" onClick={()=>{setNavactive('#')}}  className={navactive==='#'?'active':''}><AiOutlineHome/></a>
-      <a href="#about" onClick={()=>{setNavactive('#about')}} className={navactive==='#about'?'active':''}><AiOutlineUser/></a>
+      <a href="#" onClick={()=>{setNavactive('#')}}  className={navactive==='#'?'active':''}><HomeOutlinedIcon/></a>
+      <a href="#about" onClick={()=>{setNavactive('#about')}} className={navactive==='#about'?'active':''}><PersonOutlineOutlinedIcon/></a>
       <a href="#experience" onClick={()=>{setNavactive('#experience')}} className={navactive==='#experience'?'active':''}><BsBookmark/></a>
       <a href="#skills" onClick={()=>{setNavactive('#skills')}} className={navactive==='#service'?'active':''}><RiServiceLine/></a>
       <a href="#contact" onClick={()=>{setNavactive('#contact')}} className={navactive==='#contact'?'active':''}><BiMessageSquare/></a>
