425d7a7b53a0ae72b67b114fbb2ef5f43f9b3a80
rameesa <websoulrameesa@gmail.com>
2022-12-21 15:26:53 +0530

    commited

--- src/components/header/CTA.js
+++ src/components/header/CTA.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import CV from '../../assets/cv.pdf'
+import Rameesa from '../../assets/Rameesa.pdf'
 import './header.css'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} className="btn" download>Download CV</a>
+        <a href={Rameesa} className="btn" download>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
