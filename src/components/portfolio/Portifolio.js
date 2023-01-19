6052a688257e3fe46514226171cd5f8aabc288df
rameesa <websoulrameesa@gmail.com>
2023-01-19 16:30:42 +0530

    commeted

--- src/components/portfolio/Portifolio.js
+++ src/components/portfolio/Portifolio.js
@@ -8,12 +8,24 @@ import projectcareer from '../../assets/Projectcareer.png'
 import social from '../../assets/social.png'
 import Netflix from '../../assets/Netflix.png'
 import TSM from '../../assets/tsm.png'
+import quote from '../../assets/quote.png'
+import cofee from '../../assets/cofee.png'
 const Portifolio = () => {
   return (
     <section id="portfolio">
       <h5>My Recent Works</h5>
       <h2>Projects</h2>
 <div className="container portfoliocontainer">
+<article className='portfolioitem'>
+<img src={quote} className="portimg"alt="" />
+  <h3>Random Quote Generator<small>(Utilized React js)</small></h3>
+  <div className="portfolio_cta">
+  <a href="https://github.com/Rameesa96/Quotegenerator" className='btn'> Github</a>
+  <a href="https://quotegenerator-eight.vercel.app/" className='btn'>App Url</a>
+  
+  </div>
+  
+</article>
 <article className='portfolioitem'>
 <img src={projectbooking} className="portimg"alt="" />
   <h3>Booking App<small>(utilized Mern Stack)</small></h3>
@@ -38,6 +50,15 @@ const Portifolio = () => {
   <div className="portfolio_cta">
   <a href="https://github.com/Rameesa96/socialmedia" className='btn'> Github</a>
   
+  </div>
+  
+</article>
+<article className='portfolioitem'>
+<img src={cofee} className="portimg"alt="" />
+  <h3>Cofee shop(utilized React)</h3>
+  <div className="portfolio_cta">
+  <a href="https://github.com/Rameesa96/cofeeshop" className='btn'> Github</a>
+  <a href="https://cofeeshop-zgv5.vercel.app/" className='btn'> App Url</a>
   </div>
   
 </article>
@@ -86,6 +107,8 @@ const Portifolio = () => {
   </div>
   
 </article>
+
+
 
 </div>
     </section>
